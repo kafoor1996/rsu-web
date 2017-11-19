a50caf05ab48624287f110301cadcbfa67657530
kafoor1996 <abdulkafoor1996@gmail.com>
2017-11-19 14:27:17 +0530

    include file

--- js/main.js
+++ js/main.js
@@ -56,3 +56,14 @@ jQuery(function($) {
 		social_tools: false
 	});	
 });
+
+
+//include file
+
+ $(function(){
+    var includes = $('[data-include]');
+    jQuery.each(includes, function(){
+      var file = $(this).data('include') + '.html';
+      $(this).load(file);
+    });
+  });
